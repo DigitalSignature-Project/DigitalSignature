72dc23b44133328fad75e1197768fde150854ecc
Gabriel <gabkomti@gmail.com>
2026-03-30 23:45:04 +0200

    DS-08 change fetch into axios

--- frontend/src/services/rsaAPI.tsx
+++ frontend/src/services/rsaAPI.tsx
@@ -1,40 +1,23 @@
+import axios from "axios";
+
 type RsaResponse = {
   key_pub: string;
   key_priv: string;
   key_module: string;
 };
 
-export const calculateRsa = async (bits: number): Promise<RsaResponse> => {
-  const response = await fetch("http://127.0.0.1:8000/api/rsa_generate_keys", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      bits: Number(bits),
-    }),
-  });
-
-  const data: { key_pub: string; key_priv: string; key_module: string } =
-    await response.json();
+export const calculateRsa = async (bits: number) => {
+  const { data } = await axios.post<RsaResponse>(
+    "http://127.0.0.1:8000/api/rsa_generate_keys",
+    { bits },
+  );
   return data;
 };
 
-export const calculateRsaParallel = async (
-  bits: number,
-  threads: number,
-): Promise<RsaResponse> => {
-  const response = await fetch(
+export const calculateRsaParallel = async (bits: number, threads: number) => {
+  const { data } = await axios.post<RsaResponse>(
     "http://127.0.0.1:8000/api/rsa_generate_keys_parallel",
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        bits: Number(bits),
-        threads: Number(threads),
-      }),
-    },
+    { bits, threads },
   );
-
-  const data: { key_pub: string; key_priv: string; key_module: string } =
-    await response.json();
   return data;
 };
