2f0517616114ee991563975657fdb6d2e91b16a7
Gabriel Komor <gabkomti@gmail.com>
2026-04-02 22:48:01 +0200

    move files into components directory

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,37 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './Layout';
-import EncryptPage from './EncryptPage';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import EncryptPage from "./components/EncryptPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<div className="text-2xl font-bold text-slate-800">This will be the Dashboard</div>} />
+          <Route
+            index
+            element={
+              <div className="text-2xl font-bold text-slate-800">
+                This will be the Dashboard
+              </div>
+            }
+          />
           <Route path="encrypt" element={<EncryptPage />} />
-          <Route path="verify" element={<div className="text-2xl font-bold text-slate-800">Verification will be here</div>} />
-          <Route path="settings" element={<div className="text-2xl font-bold text-slate-800">Settings will be here</div>} />
+          <Route
+            path="verify"
+            element={
+              <div className="text-2xl font-bold text-slate-800">
+                Verification will be here
+              </div>
+            }
+          />
+          <Route
+            path="settings"
+            element={
+              <div className="text-2xl font-bold text-slate-800">
+                Settings will be here
+              </div>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
