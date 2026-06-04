a0ae31aff48aec98273d52b666a489782865bfb1
Gabriel Komor <gabkomti@gmail.com>
2026-06-05 01:17:18 +0200

    zmianyt

--- backend/app/core/signature.py
+++ backend/app/core/signature.py
@@ -5,21 +5,22 @@ from app.core.encryption import decrypt
 
 
 async def create_rsa_signature(file_content: str, login: str, password: str) -> str:
-    pss_config = DigiSign.RSA.PSSConfig(32, DigiSign.HASH.SHA256, DigiSign.HASH.SHA256)
+    # pss_config = DigiSign.RSA.PSSConfig(32, DigiSign.HASH.SHA256, DigiSign.HASH.SHA256)
 
-    async with httpx.AsyncClient() as client:
-        response = await client.get(
-            f"https://digital-signature-auth.digitalsignature-auth.workers.dev/api/public-key/{login}",
-            headers={"Content-Type": "application/json"},
-        )
+    # async with httpx.AsyncClient() as client:
+    #     response = await client.get(
+    #         f"https://digital-signature-auth.digitalsignature-auth.workers.dev/api/public-key/{login}",
+    #         headers={"Content-Type": "application/json"},
+    #     )
     
-    response_data = response.json()
+    # response_data = response.json()
 
-    private_key = DigiSign.BigInt.from_hex(response_data.get("encrypted_private_key", ""))
-    private_key = decrypt(private_key, password)
-    key_module = DigiSign.BigInt.from_hex(response_data.get("key_module", ""))
-    signature = DigiSign.RSA.sign(file_content, private_key, key_module, pss_config)
-    return signature.to_hex()
+    # private_key = DigiSign.BigInt.from_hex(response_data.get("encrypted_private_key", ""))
+    # private_key = decrypt(private_key, password)
+    # key_module = DigiSign.BigInt.from_hex(response_data.get("key_module", ""))
+    # signature = DigiSign.RSA.sign(file_content, private_key, key_module, pss_config)
+    # return DigiSign.Format.bytes_to_hex(signature)
+    return "test"
 
 
 async def verify_rsa_signature(file_content: str, signature: str, login: str) -> bool:
--- backend/app/routers/signature_endp.py
+++ backend/app/routers/signature_endp.py
@@ -8,22 +8,23 @@ from app.core.signature import create_rsa_signature, verify_rsa_signature
 router = APIRouter(dependencies=[Depends(verify_token)])
 
 
-@router.get("/generate_rsa_signature", response_model=RsaSignatureResponse)
+@router.post("/generate_rsa_signature", response_model=RsaSignatureResponse)
 async def generate_rsa_signature(data: RsaSignature) -> RsaSignatureResponse:
-    file_content = data.file_content
-    login = data.login
-    password = data.password
+    # file_content = data.file_content
+    # login = data.login
+    # password = data.password
 
-    signature = create_rsa_signature(file_content, login, password)
-    return RsaSignatureResponse(signature=signature)
+    # signature = await create_rsa_signature(file_content, login, password)
+    # return RsaSignatureResponse(signature=signature)
+    return RsaSignatureResponse(signature="test")
 
 
-@router.get("/verify_rsa_signature", response_model=VerifyRsaSignatureResponse)
+@router.post("/verify_rsa_signature", response_model=VerifyRsaSignatureResponse)
 async def verify_rsa_sign(data: VerifyRsaSignature) -> VerifyRsaSignatureResponse:
     file_content = data.file_content
     signature = data.signature
     login = data.login
 
-    result = verify_rsa_signature(file_content, signature, login)
+    result = await verify_rsa_signature(file_content, signature, login)
     return VerifyRsaSignatureResponse(is_valid=result)
     
