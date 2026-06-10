74479688e766db136c9a08980ca8acfc5ec66102
Gabriel <gabkomti@gmail.com>
2026-06-10 19:04:34 +0200

    update generate rsa endp

--- backend/app/core/signature.py
+++ backend/app/core/signature.py
@@ -37,10 +37,38 @@ async def create_rsa_signature(
     file_content: str,
     login: str,
     password: str,
-    encrypted_private_key: str | None = None,
-    key_module: str | None = None,
+    encrypted_private_key: str,
+    key_module: str,
+    salt_length: str,
+    hash_function_1: str,
+    hash_function_2: str
 ) -> str:
-    pss_config = DigiSign.RSA.PSSConfig(32, DigiSign.HASH.SHA256, DigiSign.HASH.SHA256)
+    try:
+        salt_length = int(salt_length)
+    except ValueError:
+        raise HTTPException(status_code=400, detail="salt_length must be an integer")
+    
+    match hash_function_1:
+        case "SHA256":
+            hash_1 = DigiSign.HASH.SHA256
+        case "SHA3_256":
+            hash_1 = DigiSign.HASH.SHA3_256
+        case "SHA3_512":
+            hash_1 = DigiSign.HASH.SHA3_512
+        case _:
+            raise ValueError("Unsupported hash function")
+        
+    match hash_function_2:
+        case "SHA256":
+            hash_2 = DigiSign.HASH.SHA256
+        case "SHA3_256":
+            hash_2 = DigiSign.HASH.SHA3_256
+        case "SHA3_512":
+            hash_2 = DigiSign.HASH.SHA3_512
+        case _:
+            raise ValueError("Unsupported hash function")
+
+    pss_config = DigiSign.RSA.PSSConfig(salt_length, hash_1, hash_2)
 
     if not encrypted_private_key or not key_module:
         keys = await _fetch_user_keys(login)
--- backend/app/routers/signature_endp.py
+++ backend/app/routers/signature_endp.py
@@ -10,16 +10,15 @@ router = APIRouter(dependencies=[Depends(verify_token)])
 
 @router.post("/generate_rsa_signature", response_model=RsaSignatureResponse)
 async def generate_rsa_signature(data: RsaSignature) -> RsaSignatureResponse:
-    file_content = data.file_content
-    login = data.login
-    password = data.password
-
     signature = await create_rsa_signature(
-        file_content,
-        login,
-        password,
+        data.file_content,
+        data.login,
+        data.password,
         data.encrypted_private_key,
         data.key_module,
+        data.salt_length,
+        data.hash_function_1,
+        data.hash_function_2
     )
     return RsaSignatureResponse(signature=signature)
 
